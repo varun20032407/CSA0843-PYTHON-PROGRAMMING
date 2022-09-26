b2e69669a8d5f2ea5cb573b9c5df2b3cd08a8ea1
varun20032407 <112461609+varun20032407@users.noreply.github.com>
2022-09-26 10:56:21 +0530

    Add files via upload

--- isostr.py
+++ isostr.py
@@ -1,20 +1,17 @@
-a=input("ENTER THE FIRST STIRNG---> ")
-b=input("ENTER THE SECOND STRING---> ")
-m=200
-c=len(a)
-d=len(b)
-if(c!=d):
-    print(False)
-x=[False]*m
-map=[-1]*m
-for i in range(b):
-    if(map[ord(a[i])]==-1):
-        if(map[ord(b[i])])==True:
-           print(False)
-        x[ord(b[i])]=True
-        map[ord(a[i])]=b[i]
-    elif(map[ord(a[i])])!=b[i]:
-        print(False)
-print(True)
-        
-
+def im(a,b):
+    if len(a)!=len(b):
+        return False
+    else:
+        m1,m2={},{}
+        for i in range(len(a)):
+            c,ch=a[i],b[i]
+            if(c not in m1):
+                m1[c]=ch
+            if(ch not in m2):
+                m2[ch]=c
+            if(m1[c]!=ch)or(m2[ch]!=c):
+                return False
+    return True
+a=input("S--> ")
+b=input("T--> ")
+print(im(a,b))
