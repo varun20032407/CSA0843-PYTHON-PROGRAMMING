cce5eac929e4cb108a96e27858db6b4637b0ab27
varun20032407 <112461609+varun20032407@users.noreply.github.com>
2022-09-29 10:37:00 +0530

    Add files via upload

---
+++ vo.py
@@ -0,0 +1,6 @@
+s=input("ENTER THE STRING----> ")
+vowel=0
+for i in s:
+    if(i=='a' or i=='e' or i=='i' or i=='o' or i=='u' or i=='A' or i=='E' or i=='I' or i=='O' or i=='U'):
+        vowel=vowel+1
+print("NUMBER OF VOWELS---> ",vowel)
